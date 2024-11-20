415c9ee6dd771dcd315831543c65e33bc351865c
RohitoOo <bhambhani.ba@gmail.com>
2024-11-20 18:30:04 -0500

    Created Session Action

--- app/actions/createSession.js
+++ app/actions/createSession.js
@@ -1,7 +1,7 @@
 "use server";
 
 import { createAdminClient } from "@/config/appwrite";
-
+import { cookies } from "next/headers";
 async function createSession(previousState, formData) {
   const email = formData.get("email");
   const password = formData.get("password");
@@ -16,9 +16,31 @@ async function createSession(previousState, formData) {
 
   // GET ACCOUNT INSTANCE
 
-  return {
-    success: true,
-  };
+  const { account } = await createAdminClient();
+
+  try {
+    // Generate Session
+    console.log(email, password);
+    const session = await account.createEmailPasswordSession(email, password);
+
+    // Create Cookie
+    cookies().set("appwrite-session", session.secret, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      expires: new Date(session.expire),
+      path: "/",
+    });
+
+    return {
+      success: true,
+    };
+  } catch (error) {
+    console.log("Authentication Error: ", error);
+    return {
+      error: "Invalid Credentials ",
+    };
+  }
 }
 
 export default createSession;
