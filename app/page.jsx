9a9eed49db576dcc36560caf70f1f8f3c93c8c13
RohitoOo <bhambhani.ba@gmail.com>
2024-11-20 12:27:44 -0500

    Plugged in DB AppWrite

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,15 @@
-import rooms from "@/data/rooms.json";
+// import rooms from "@/data/rooms.json";
 import RoomCard from "@/components/RoomCard";
 import Heading from "@/components/Heading";
-export default function Home() {
+import getAllRooms from "./actions/getAllRooms";
+export default async function Home() {
+  const rooms = await getAllRooms();
+  // console.log("ROOMS ::", rooms);
   return (
     <>
       <Heading title="Available Rooms" />
       {rooms.length > 0
-        ? rooms.map((room) => <RoomCard room={room} />)
+        ? rooms.map((room) => <RoomCard room={room} key={room.$id} />)
         : "No Rooms Available"}
     </>
   );
