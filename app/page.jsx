9760619ca6c1bf0b3dd697e746a89542b548adad
RohitoOo <bhambhani.ba@gmail.com>
2024-11-19 20:42:09 -0500

    Initial Commit

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import rooms from "@/data/rooms.json";
+import RoomCard from "@/components/RoomCard";
+import Heading from "@/components/Heading";
+export default function Home() {
+  return (
+    <>
+      <Heading title="Available Rooms" />
+      {rooms.length > 0
+        ? rooms.map((room) => <RoomCard room={room} />)
+        : "No Rooms Available"}
+    </>
+  );
+}
