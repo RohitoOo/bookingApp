623ad16d91cdf958940ebfe68bbe8bb5ba083210
RohitoOo <bhambhani.ba@gmail.com>
2024-11-25 15:58:54 -0500

    Added Bookings Page and Action

--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import getMyBookings from "@/app/actions/getMyBookings";
+import Heading from "@/components/Heading";
+import BookedRoomCard from "@/components/BookedRoomCard";
+const BookingsPage = async () => {
+  const bookings = await getMyBookings();
 
-const BookingsPage = () => {
-  return <div>Bookings Page</div>;
+  return (
+    <>
+      {bookings.length === 0
+        ? console.log("No bookings")
+        : bookings.map((booking) => {
+            console.log(booking);
+            return <BookedRoomCard booking={booking} />;
+          })}
+    </>
+  );
 };
 
 export default BookingsPage;
